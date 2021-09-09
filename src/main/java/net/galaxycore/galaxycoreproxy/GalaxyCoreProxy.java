eba666a4d89b5eef1b7fdbd9b179779355e1a25d
Flo-Mit-H <flogamesg@gmail.com>
2021-09-09 16:39:09 +0200

    initial commit

---
+++ src/main/java/net/galaxycore/galaxycoreproxy/GalaxyCoreProxy.java
@@ -0,0 +1,25 @@
+package net.galaxycore.galaxycoreproxy;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "galaxycoreproxy",
+        name = "GalaxyCoreProxy",
+        version = "1.0-SNAPSHOT",
+        description = "Proxy Plugin for GalaxyCore.net",
+        url = "https://galaxycore.net",
+        authors = {"Flo-Mit-H"}
+)
+public class GalaxyCoreProxy {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
