90f9381ecda505d2408a9b25cc206b6c7c74e7be
Flo-Mit-H <flogamesg@gmail.com>
2021-09-20 20:41:30 +0200

    MC-78 Block Rules COMMAND (not TabCompletion) MC-78 add Commands to Blacklist in TabCompletion

--- src/main/java/net/galaxycore/galaxycoreproxy/listener/PluginCommandListener.java
+++ src/main/java/net/galaxycore/galaxycoreproxy/listener/PluginCommandListener.java
@@ -2,9 +2,13 @@ package net.galaxycore.galaxycoreproxy.listener;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.command.CommandExecuteEvent;
+import com.velocitypowered.api.proxy.Player;
+import net.galaxycore.galaxycoreproxy.configuration.PlayerLoader;
 import net.galaxycore.galaxycoreproxy.configuration.ProxyProvider;
 import net.galaxycore.galaxycoreproxy.utils.MessageUtils;
 import net.kyori.adventure.text.Component;
+
+import java.awt.event.ComponentEvent;
 
 public class PluginCommandListener {
 
@@ -14,12 +18,28 @@ public class PluginCommandListener {
 
     @Subscribe
     public void onPlayerExecutePluginCommand(CommandExecuteEvent event) {
+        // Check if Command checks plguins to hand down to Server
         if(ProxyProvider.getProxy().getPluginCommand().getCommands().contains(event.getCommand())) {
             if(event.getCommandSource().hasPermission("velocity.command.plugins"))
                 event.setResult(CommandExecuteEvent.CommandResult.command("velocity plugins"));
             else
                 event.getCommandSource().sendMessage(Component.text("§fProxyPlugins (9): "
                         + MessageUtils.getI18NMessage(event.getCommandSource(), "proxy.command.plugins.no_permission")));
+        }
+
+        //TODO IMPORTANT: BELOW THIS IF STATEMENT YOU CAN ONLY PROCESS EVETS WHERE THE EXECUTOR HAS TO BE A PLAYER
+        if(!(event.getCommandSource() instanceof Player)) {
+            event.getCommandSource().sendMessage(Component.text("§cThis command is only available for Players"));
+            return;
+        }
+
+        // Check if Player has accepted rules or not
+        if(PlayerLoader.load((Player) event.getCommandSource()) != null) {
+            if(!event.getCommand().startsWith("rules"))
+                event.setResult(CommandExecuteEvent.CommandResult.denied());
+        }else {
+            if(event.getCommand().startsWith("rules"))
+                event.setResult(CommandExecuteEvent.CommandResult.denied());
         }
 
     }
--- src/main/java/net/galaxycore/galaxycoreproxy/tabcompletion/TabCompletionListener.java
+++ src/main/java/net/galaxycore/galaxycoreproxy/tabcompletion/TabCompletionListener.java
@@ -5,6 +5,9 @@ import com.velocitypowered.api.event.command.PlayerAvailableCommandsEvent;
 import net.galaxycore.galaxycoreproxy.configuration.PlayerLoader;
 import net.galaxycore.galaxycoreproxy.configuration.ProxyProvider;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TabCompletionListener {
 
     public TabCompletionListener() {
@@ -17,6 +20,10 @@ public class TabCompletionListener {
         event.getRootNode().getChildren().removeIf((command) -> command.getName().contains(":"));
         if(PlayerLoader.load(event.getPlayer()) != null)
             event.getRootNode().getChildren().removeIf(command -> command.getName().equals("rules"));
+
+        List<String> blackListedCommands = Arrays.asList(ProxyProvider.getProxy().getProxyNamespace().get("proxy.commandblacklist").toLowerCase().split("\\|"));
+        event.getRootNode().getChildren().removeIf(command -> blackListedCommands.contains(command.getName().toLowerCase())
+                && !event.getPlayer().hasPermission("proxy.commandblacklist.bypass"));
     }
 
 }
